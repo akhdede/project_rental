00e4086d9c7eab805f5f5b874b665d972fd6b229
akhdede <akhdede@gmail.com>
2018-04-25 13:03:22 +0800

    update on 25.04.2018@01.03pm

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -26,58 +26,116 @@ class Order extends CI_Controller {
 
     public function kursi_order($url)
     {
+        // menangkap plat nomor pada url
         $where = array('plat_nomor' => $url);
         $where2 = array(
             'plat_nomor' => $url,
             'status_order' => 1
         );
+
+        // fungsi database untuk menampilkan mobil yang tersedia berdasarkan url yang ditangkap
         $mobil_tersedia = $this->db->get_where('mobil_tersedia', $where)->result();
+
+        // fungsi database untuk menampilkan harga sewa
         $kursi_harga = $this->db->get('kursi_harga')->result();
 
         foreach($mobil_tersedia as $mt) {
 
             echo'
-            <div class="col-md-8 col-sm-12 album-show">';
-                echo'
-                <span class="album-light">
-                    <div class="card mb-4 mb-12 box-shadow">
-                        <img class="card-img-top" src="'.base_url('assets/img/avanza.jpg').'" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">';
-                        $cek_full = $this->db->get_where('kursi_tersedia', $where2)->num_rows();
-                        if($cek_full < 7){
-                            echo '<img class="album-background2" src="'.base_url('assets/img/album-background.png').'" alt="">';
-                        }else{
-                            echo '<img class="album-background2" src="'.base_url('assets/img/album-background-sold.png').'" alt="">';
-                        }
-                        echo'
-                        <div class="card-body">
-                            <span class="card-text font-weight-bold text-dark">
-                                <h2 class="text-center">'.$mt->merek.' ('.$mt->plat_nomor.')</h2>
-                            </span><br />
-                            <h5>';
-                                $kursi_tersedia = $this->db->get_where('kursi_tersedia', array('plat_nomor' => $mt->plat_nomor))->result();
-                                foreach($kursi_tersedia as $kt){
-                                    echo '&nbsp;&nbsp;<span class="text-secondary"> Kursi '.$kt->nomor_kursi.' : </span>';
-                                    if($kt->status_order == 0) 
-                                        echo '<span class="fa fa-check text-success"></span> <button class="btn btn-primary btn-sm" style="margin-bottom: 5px;">pesan</button><br>'; 
-                                    else  
-                                        echo '<span class="fa fa-times text-danger"></span> <button class="btn btn-secondary btn-sm" disabled style="margin-bottom: 5px;">dipesan</button><br>' ;
-                                }
+            <div class="col-md-12">
+                <div class="col-md-8 col-sm-12 album-show">
+                    <span class="album-light">
+                        <div class="card mb-4 mb-12 box-shadow">
+                            <img class="card-img-top" src="'.base_url('assets/img/avanza.jpg').'" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">';
+                            // fungsi database untuk menghitung jumlah kursi yang dipesan berdasarkan url yang ditangkap
+                            $cek_full = $this->db->get_where('kursi_tersedia', $where2)->num_rows();
+                            // jika total kursi yang dipesan lebih dari 6 maka akan menampilkan gambar sold out
+                            if($cek_full > 6){
+                                echo '<img class="album-background2" src="'.base_url('assets/img/album-background-sold.png').'" alt="" style="z-index: 2000000;">';
+                            }
+                            echo'
+                            <div class="card-body">
+                                <span class="card-text font-weight-bold text-dark">
+                                    <h2 class="text-center">'.$mt->merek.' ('.$mt->plat_nomor.')</h2>
+                                </span><br />
+                                <h6>
+                                    <div class="album-background2">';
+                                        // fungsi database untuk menampilkan kursi yang tersedia berdasarkan url yang ditangkap
+                                        $kursi_tersedia = $this->db->get_where('kursi_tersedia', array('plat_nomor' => $mt->plat_nomor))->result();
+                                        // menampilkan button posisi kursi pertama
+                                        foreach($kursi_tersedia as $kt){
+                                            if($kt->nomor_kursi == 1){
+                                                if($kt->status_order == 0)
+                                                    // jika kursi belum dipesan akan menampilkan button primary
+                                                    echo '<button class="btn btn-primary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
+                                                else
+                                                    // jika kursi sudah dipesan akan menampilkan button secondary
+                                                    echo '<button class="btn btn-secondary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
 
-                            echo'
-                            </h5>
-                            <hr>
-                            <small class="text-muted">
-                                <span class="text-secondary">Harga sewa : </span><br />';
-                                    foreach($kursi_harga as $kh){
-                                        echo '<span class="text-secondary">- Kursi '.$kh->posisi.'<i> ( '.$kh->keterangan.' )</i> - Rp.'.number_format("$kh->harga","0",",",".").' </span><br />';
+                                            }
+                                        }
+                                        echo '<br>';
+                                        // menampilkan button posisi kursi kedua sampai keempat
+                                        foreach($kursi_tersedia as $kt){
+                                            if(($kt->nomor_kursi > 1)  and ($kt->nomor_kursi < 5)){
+                                                if($kt->status_order == 0)
+                                                    // jika kursi belum dipesan akan menampilkan button primary
+                                                    echo '<button class="btn btn-primary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
+                                                else
+                                                    // jika kursi sudah dipesan akan menampilkan button secondary
+                                                    echo '<button class="btn btn-secondary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
+                                            }
+                                        }
+                                        echo '<br>';
+                                        // menampilkan button posisi kursi kelima sampai ketujuh
+                                        foreach($kursi_tersedia as $kt){
+                                            if(($kt->nomor_kursi > 4)  and ($kt->nomor_kursi < 8)){
+                                                if($kt->status_order == 0)
+                                                    // jika kursi belum dipesan akan menampilkan button primary
+                                                    echo '<button class="btn btn-primary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
+                                                else
+                                                    // jika kursi sudah dipesan akan menampilkan button secondary
+                                                    echo '<button class="btn btn-secondary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
+                                            }
+                                        }
+                                    echo'
+                                    </div>';
+
+                                    // menampilkan status nomor kursi
+                                    foreach($kursi_tersedia as $kt){
+                                        echo '&nbsp;&nbsp;<span class="text-secondary"> Kursi '.$kt->nomor_kursi.' : </span>';
+                                        if($kt->status_order == 0) 
+                                            // jika kursi belum dipesan akan menampilkan checklist berwarna hijau dan button primary pesan
+                                            echo '<span class="fa fa-check text-success"></span> <button class="btn btn-primary btn-sm" style="margin-bottom: 3px;">pesan</button><br>'; 
+                                        else  
+                                            // jika kursi sudah dipesan akan menampilkan cross berwarna merah dan button secondary dipesan
+                                            echo '<span class="fa fa-times text-danger"></span> <button class="btn btn-secondary btn-sm" disabled style="margin-bottom: 3px;">dipesan</button><br>' ;
                                     }
-                            echo'
-                            </small>
+
+                                echo'
+                                </h6>
+                                <hr>
+                                <small class="text-muted">
+                                    <span class="text-secondary">Harga sewa : </span><br />';
+                                        // menampilkan keterangan harga sewa
+                                        foreach($kursi_harga as $kh){
+                                            echo '<span class="text-secondary">- Kursi '.$kh->posisi.'<i> ( '.$kh->keterangan.' )</i> - Rp.'.number_format("$kh->harga","0",",",".").' </span><br />';
+                                        }
+                                echo'
+                                </small>
+                            </div>
                         </div>
-                    </div>
-                </span>
+                    </span>
+                </div>
             </div>';
         }
+
+        echo'
+        <div class="col-md-12">
+            <div class="col-md-4 col-sm-12 album-show">
+                hahaha
+            </div>
+        </div>';
     }
 
 }
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,23 +20,28 @@ class Welcome extends CI_Controller {
 
     public function kursi_tersedia()
     {
+        // fungsi database untuk menampilkan mobil yang tersedia
         $mobil_tersedia = $this->db->get('mobil_tersedia')->result();
+        // fungsi database untuk menampilkan harga sewa 
         $kursi_harga = $this->db->get('kursi_harga')->result();
 
         foreach($mobil_tersedia as $mt) {
 
             echo'
             <div class="col-md-4 col-sm-12 album-show">';
+                // fungsi database untuk menghitung jumlah kursi yang di pesan
                 $cek_full = $this->db->get_where('kursi_tersedia', array('plat_nomor' => $mt->plat_nomor, 'status_order' => 1))->num_rows();
 
+                // jika jumlah kursi yang dipesan kurang dari 7 maka button pesan akan ditampilkan
                 if($cek_full < 7){
                     echo '<a href="'.base_url('order/mobil/').strtolower($mt->plat_nomor).'" class="btn btn-danger btn-lg"><span class="fa fa-shopping-cart"></span> Pesan</a>';
                 }
                 echo'
                 <span class="album-light">
                     <div class="card mb-4 mb-12 box-shadow">
-                        <img class="card-img-top" src="'.base_url('assets/img/avanza.jpg').'" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">';
+                        <img class="card-img-top" src="'.base_url('assets/img/avanza.jpg').'" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">'; #gambar mobil tersedia
 
+                        // jika total kursi yang dipesan sudah lebih dari 6 maka akan menampilkan gambar sold out
                         if($cek_full > 6){
                             echo '<img class="album-background" src="'.base_url('assets/img/album-background-sold.png').'" alt="" style="z-index: 20000;">';
                         }
@@ -46,33 +51,43 @@ class Welcome extends CI_Controller {
                             <span class="card-text font-weight-bold text-dark">
                                 <h4 class="text-center">'.$mt->merek.' ('.$mt->plat_nomor.')</h4>
                             </span><br />
-                            <h5>
+                            <h6>
                                 <div class="album-background">';
+                                    // fungsi database untuk menampilkan kursi yang tersedia
                                     $kursi_tersedia = $this->db->get_where('kursi_tersedia', array('plat_nomor' => $mt->plat_nomor))->result();
+                                    // menampilkan button posisi kursi pertama
                                     foreach($kursi_tersedia as $kt){
                                         if($kt->nomor_kursi == 1){
                                             if($kt->status_order == 0)
+                                                // jika kursi belum dipesan akan menampilkan button primary
                                                 echo '<button class="btn btn-primary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
                                             else
+                                                // jika kursi sudah dipesan akan menampilkan button secondary
                                                 echo '<button class="btn btn-secondary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
 
                                         }
                                     }
                                     echo '<br>';
+                                    // menampilkan button posisi kursi kedua sampai keempat
                                     foreach($kursi_tersedia as $kt){
                                         if(($kt->nomor_kursi > 1)  and ($kt->nomor_kursi < 5)){
                                             if($kt->status_order == 0)
+                                                // jika kursi belum dipesan akan menampilkan button primary
                                                 echo '<button class="btn btn-primary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
                                             else
+                                                // jika kursi sudah dipesan akan menampilkan button secondary
                                                 echo '<button class="btn btn-secondary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
                                         }
                                     }
                                     echo '<br>';
+                                    // menampilkan button posisi kursi kelima sampai ketujuh
                                     foreach($kursi_tersedia as $kt){
                                         if(($kt->nomor_kursi > 4)  and ($kt->nomor_kursi < 8)){
                                             if($kt->status_order == 0)
+                                                // jika kursi belum dipesan akan menampilkan button primary
                                                 echo '<button class="btn btn-primary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
                                             else
+                                                // jika kursi sudah dipesan akan menampilkan button secondary
                                                 echo '<button class="btn btn-secondary btn-lg" disabled style="margin: 3px;">'.$kt->nomor_kursi.'</button>'; 
                                         }
                                     }
@@ -80,19 +95,23 @@ class Welcome extends CI_Controller {
                                 </div>';
                                     
 
+                                // menampilkan status nomor kursi
                                 foreach($kursi_tersedia as $kt){
                                     echo '&nbsp;&nbsp;<span class="text-secondary"> Kursi '.$kt->nomor_kursi.' : </span>';
                                     if($kt->status_order == 0) 
-                                        echo '<span class="fa fa-check text-success"></span><br>'; 
+                                        // jika kursi belum dipesan akan menampilkan checklist berwarna hijau
+                                        echo '<span class="fa fa-check text-success" style="margin-bottom: 7px;"></span><br>'; 
                                     else  
-                                        echo '<span class="fa fa-times text-danger"></span><br>' ;
+                                        // jika kursi sudah dipesan akan menampilkan cross berwarna merah
+                                        echo '<span class="fa fa-times text-danger" style="margin-bottom: 7px;"></span><br>' ;
                                 }
 
                             echo'
-                            </h5>
-                            <br><hr>
+                            </h6>
+                            <hr>
                             <small class="text-muted">
                                 <span class="text-secondary">Harga sewa : </span><br />';
+                                    // menampilkan keterangan harga sewa
                                     foreach($kursi_harga as $kh){
                                         echo '<span class="text-secondary">- Kursi '.$kh->posisi.'<i> ( '.$kh->keterangan.' )</i> - Rp.'.number_format("$kh->harga","0",",",".").' </span><br />';
                                     }
