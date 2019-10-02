213f9090a59ea1bcb2197789490789d848322901
akhdede <akhdede@gmail.com>
2019-10-03 00:11:44 +0800

    change header text color to white

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -27,12 +27,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <div class="row">
                     <div class="col-sm-8 col-md-7 py-4">
                         <h4 class="text-white">About</h4>
-                        <p class="text-black">CV. New Garuda Totabuan adalah UKM yang bergerak dibidang jasa transportasi.</p>
+                        <p class="text-white">CV. New Garuda Totabuan adalah UKM yang bergerak dibidang jasa transportasi.</p>
                     </div>
                     <div class="col-sm-4 offset-md-1 py-4">
                         <h4 class="text-white">Contact</h4>
                         <ul class="list-unstyled">
-                            <li><span class="text-black">081524605468</span></li>
+                            <li><span class="text-white">081524605468</span></li>
                         </ul>
                     </div>
                 </div>
